0ff15d5cfd7c6c970472731ef6ff34008c742182
SirArnoldB <legendaryanoldb@gmail.com>
2021-12-08 01:30:58 -0600

    update

--- Swap Nodes In Pairs.py
+++ Swap Nodes In Pairs.py
@@ -22,35 +22,39 @@ Source: https://leetcode.com/problems/swap-nodes-in-pairs/
     >>> The number of nodes in the list is in the range [0, 100].
     >>> 0 <= Node.val <= 100
 '''
+
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution(object):
     def swapPairs(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        # Initialize everything 
+        # Initialize everything
         dummyNode = ListNode(0, head)
         # the node before the next pair of numbers
         beforeNode = dummyNode
-        
+
         # for each pair of adjacent nodes
-        while beforeNode.next != None and beforeNode.next.next: # as long as there are two nodes ahead
-            # set up temporary pointers to the two nodes 
+        while beforeNode.next != None and beforeNode.next.next:  # as long as there are two nodes ahead
+            # set up temporary pointers to the two nodes
             firstNode = beforeNode.next
             secondNode = beforeNode.next.next
-            
+
             # do pointer re-arrangement
             beforeNode.next = secondNode
             firstNode.next = secondNode.next
             secondNode.next = firstNode
-            
-            # Move everything forward to the next pair 
+
+            # Move everything forward to the next pair
             beforeNode = firstNode
-        
+
         # dummyNode.next is the beginning of the re-arranged list
         return dummyNode.next
