ba02f41da8dbb0647ab6c11e29e1b198c8078e78
SirArnoldB <legendaryanoldb@gmail.com>
2021-12-08 00:30:04 -0600

    update

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -49,8 +49,9 @@ class Solution:
             - If we encounter an opening bracket, we simply push it onto the stack. 
             - This means we will process it later, let us simply move onto the sub-expression ahead.
             - If we encounter a closing bracket, then we check the element on top of the stack. 
-            - If the element at the top of the stack is an opening bracket of the same type, then we               pop it off the stack and continue processing. Else, this implies an invalid expression.
-            - In the end, if we are left with a stack still having elements, then this implies an                   invalid expression.
+            - If the element at the top of the stack is an opening bracket of the same type, then we               
+            - pop it off the stack and continue processing. Else, this implies an invalid expression.
+            - In the end, if we are left with a stack still having elements, then this implies an invalid expression.
         '''
         
         # stack to keep track of opening brackets
