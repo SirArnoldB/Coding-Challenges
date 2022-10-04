6eceb4d7a74f5129f5dd4a429388f3984213edf6
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2022-10-04 11:40:10 -0500

    Update Add Strings.py

--- Python Solutions/Add Strings.py
+++ Python Solutions/Add Strings.py
@@ -2,7 +2,7 @@
 ## Add Strings
 --------------
 Source: https://leetcode.com/problems/add-strings/
-- Given two non-negative integers, num1 and num2 represultant_sumented as string, return the sum of num1 and num2 as a string.
+- Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
 - You must solve the problem without using any built-in library for handling large integers (such as BigInteger). 
 - You must also not convert the inputs to integers directly.
 
